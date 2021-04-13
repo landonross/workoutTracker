b3f34dc6422a834f2deeabf121392bf54b2ece15
Landon Ross <landon.w.ross@gmail.com>
2021-04-13 12:30:21 -0600

    added api/html routes. They still don't work correctly, but they are close

---
+++ models/exerciseTracker.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        unique: true,
+        required: true
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      }
+    }]
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+
+module.exports = workout;
---
+++ routes/api-routes.js
@@ -0,0 +1,83 @@
+
+const db = require("../models");
+var express = require('express');
+var app = express();
+
+module.exports = (app) => {
+    app.get("/api/workouts", (req, res) => {
+        db.WorkOut.aggregate(
+            [
+
+                {
+                    $addFields: { totalDuration: { $sum: "$exercises.duration" } }
+                }
+            ])
+
+            .then(workoutDB => {
+                res.json(workoutDB);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.WorkOut.aggregate(
+            [
+                {
+                    $addFields: {
+                        totalDuration: { $sum: "$exercises.duration" },
+                        totalDuration: { $sum: "$exercises.duration", },
+                        totalWeight: { $sum: "$exercises.weight" },
+                        totalSets: { $sum: "$exercises.sets" },
+                        totalReps: { $sum: "$exercises.reps" },
+                        totalDistance: { $sum: "$exercises.distance" }
+                    }
+                },
+                { "$sort": { _id: -1 } },
+                { "$limit": 7 }
+
+            ]
+        )
+            .then(workoutDB => {
+                console.log(workoutDB)
+                res.json(workoutDB);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    })
+
+    app.put("/api/workouts/:id", (req, res) => {
+        let id = req.params.id
+        if (req.body.type == "cardio") {
+
+            db.WorkOut.findByIdAndUpdate(id, { $push: { exercises: req.body } })
+                .then(workoutDB => {
+                    console.log(workoutDB)
+                    res.json(workoutDB);
+                })
+                .catch(err => {
+                    res.json(err);
+                })
+        } else if (req.body.type === "resistance") {
+            db.WorkOut.findByIdAndUpdate(id, { $push: { exercises: req.body } })
+                .then(workoutDB => {
+                    res.json(workoutDB);
+                })
+                .catch(err => {
+                    res.json(err);
+                })
+        }
+    })
+
+    app.post("/api/workouts", ({ body }, res) => {
+        db.WorkOut.create({})
+            .then(workoutDB => {
+                res.json(workoutDB);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+}
